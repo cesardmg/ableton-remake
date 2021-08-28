5b9229a7e1ce74cec9e656a1bfdd5a4f48d4ec96
Cesar Camacho <icesardcm99@gmail.com>
2021-08-27 20:02:45 -0500

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      abletonOrange: "#FF764D",
+      abletonBlue: "#0000FF",
+    },
+    fontSize: {
+      mediumFont: "17px",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};

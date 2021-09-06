99e86948b10e5da044a0c397cb42f56414e5bb4a
Cesar Camacho <icesardcm99@gmail.com>
2021-09-06 18:51:16 -0500

    Finishe adding youtube video dummy image and 2nd paragraph

--- pages/index.js
+++ pages/index.js
@@ -93,18 +93,18 @@ export default function Home() {
           </div>
         </div>
 
-        <div id="Content" className="mx-auto max-w-7xl">
+        <section id="Content" className="mx-auto max-w-7xl">
           <div
             id="First Paragraph"
             className="flex flex-col gap-5 mx-auto mt-24 max-w-prose"
           >
-            <p className="font-semibold leading-relaxed text-pxl">
+            <h2 className="font-semibold leading-relaxed text-pxl">
               We make <span className="text-abletonBlue">Live</span>,{" "}
               <span className="text-abletonBlue">Push</span> and
               <span className="text-abletonBlue"> Link</span> — unique software
               and hardware for music creation and performance. With these
               products, our community of users creates amazing things.
-            </p>
+            </h2>
             <p className="text-lg">
               Ableton was founded in 1999 and released the first version of Live
               in 2001. Our products are used by a community of dedicated
@@ -113,42 +113,73 @@ export default function Home() {
           </div>
 
           <div
-            id="First Paragraph"
-            className="flex flex-col gap-5 mx-auto mt-24 max-w-prose"
+            id="Lemonade Images"
+            className="relative flex flex-row mx-auto mt-32 h-100"
           >
-            <p className="font-semibold leading-relaxed text-pxl">
-              We make <span className="text-abletonBlue">Live</span>,{" "}
-              <span className="text-abletonBlue">Push</span> and
-              <span className="text-abletonBlue"> Link</span> — unique software
-              and hardware for music creation and performance. With these
-              products, our community of users creates amazing things.
-            </p>
-            <p className="text-lg">
-              Ableton was founded in 1999 and released the first version of Live
-              in 2001. Our products are used by a community of dedicated
-              musicians, sound designers, and artists from across the world.
-            </p>
-          </div>
-          <div> </div>
-
-          <div className="relative flex flex-row mx-auto mt-32 h-100">
-            <div className="relative inline-block w-5/12 h-full bg-gray-300 ">
+            <div className="relative z-20 inline-block ">
               <img
                 alt="Person producing music"
                 src="images/ableton-img-2.jpg"
                 className=""
               />
             </div>
-            <div className="relative inline-block w-7/12 h-100">
+            <div className="relative z-10 inline-block ">
               <img
                 alt="Person producing music"
                 src="images/ableton-img-3.jpg"
-                className=""
+                className="object-cover"
               />
             </div>
-            <div className="absolute w-7/12 bg-opacity-25 h-100 bg-lemonade "></div>
+            <div className="absolute inset-y-0 right-0 z-0 w-7/12 h-100 bg-lemonade"></div>
           </div>
-        </div>
+
+          <div
+            id="2nd Paragraph"
+            className="flex flex-col gap-5 mx-auto mt-24 max-w-prose"
+          >
+            <h2 className="font-semibold leading-relaxed text-pxl">
+              Making music isn’t easy. It takes time, effort, and learning. But
+              when you’re in the flow, it’s incredibly rewarding.
+            </h2>
+            <p className="text-lg">
+              We feel the same way about making Ableton products. The driving
+              force behind Ableton is our passion for what we make, and the
+              people we make it for.
+            </p>
+          </div>
+
+          <div
+            id="Dummy Video Image"
+            className="items-center w-1/2 mx-auto mt-24 "
+          >
+            <div>
+              <img className="relative" src="images/video-image.png"></img>
+            </div>
+            <p className="mt-1 text-xs font-semibold">
+              Why Ableton - Juanpe Bolivar
+            </p>
+          </div>
+
+          <div
+            id="3rd Paragraph"
+            className="flex flex-col gap-5 mx-auto mt-24 max-w-prose"
+          >
+            <h2 className="font-semibold leading-relaxed text-pxl">
+              We are more than 350 people from 30 different countries divided
+              between our headquarters in Berlin and our offices in Los Angeles
+              and Tokyo.
+            </h2>
+            <p className="text-lg">
+              Most of us are active musicians, producers, and DJs, and many of
+              us use Live and Push every day. We come from a wide range of
+              cultural and professional backgrounds. Some of us have PhDs, some
+              are self-taught, and most of us are somewhere in between. What
+              connects us is the shared belief that each of us has the skills
+              and knowledge to contribute to something big: helping to shape the
+              future of music culture.
+            </p>
+          </div>
+        </section>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 pt-60 mb-96 mt-96">

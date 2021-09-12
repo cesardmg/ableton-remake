ac24418600a3df434d9c0cb3a509c7c7d9a22629
Cesar Camacho <icesardcm99@gmail.com>
2021-09-12 13:57:59 -0500

    Finished footer

---
+++ components/Chevron.jsx
@@ -0,0 +1,8 @@
+export default function Chevron() {
+  return (
+    <img
+      className="inline-flex w-3 h-3 ml-1 "
+      src="images/icons/chevron.svg"
+    ></img>
+  );
+}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,5 @@
+import Chevron from "./Chevron";
+
 export default function Footer() {
   return (
     <div className="w-5/6 mx-auto mb-32 max-w-7xl mt-96">
@@ -12,15 +14,24 @@ export default function Footer() {
       <div className="flex justify-between mb-16 leading-8 text-mediumFont">
         <div className="flex justify-between w-2/3 ">
           <div className="flex flex-col ">
-            <a>Register Live or Push </a>
-            <a>About Ableton</a>
-            <a>Jobs</a>
+            <a>
+              Register Live or Push
+              <Chevron />
+            </a>
+            <a>
+              About Ableton
+              <Chevron />
+            </a>
+            <a>
+              Jobs
+              <Chevron />
+            </a>
             <div className="flex pt-2 gap-x-2">
               <a href="#">
                 <img
                   className="w-10 h-auto"
                   src="/images/icons/facebook-icon.png"
-                ></img>{" "}
+                ></img>
               </a>
               <a href="#">
                 <img
@@ -44,9 +55,18 @@ export default function Footer() {
           </div>
           <div className="flex flex-col w-3/6 leading-7 ">
             <h3 className="font-semibold">Education</h3>
-            <a>Offers for students and teachers</a>
-            <a>Ableton for the Classroom</a>
-            <a>Ableton for Colleges and Universities</a>
+            <a>
+              Offers for students and teachers
+              <Chevron />
+            </a>
+            <a>
+              Ableton for the Classroom
+              <Chevron />
+            </a>
+            <a>
+              Ableton for Colleges and Universities
+              <Chevron />
+            </a>
           </div>
         </div>
         <div className="flex flex-col w-2/5 ">
@@ -67,25 +87,51 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="flex justify-between mb-20 leading-8 text-mediumFont">
+      <div className="flex justify-between mb-16 leading-8 text-mediumFont">
         <div className="flex justify-between w-2/3 ">
           <div className="flex flex-col ">
             <h3 className="font-semibold">Community</h3>
-            <a>Find Ableton User Groups</a>
-            <a>Find Certified Training</a>
-            <a>Become a Certified Trainer</a>
+            <a>
+              Find Ableton User Groups
+              <Chevron />
+            </a>
+            <a>
+              Find Certified Training
+              <Chevron />
+            </a>
+            <a>
+              Become a Certified Trainer
+              <Chevron />
+            </a>
           </div>
           <div className="flex flex-col w-3/6 leading-7">
             <h3 className="font-semibold">Distributors</h3>
-            <a>Find Distributors</a>
-            <a>Try Push in-store</a>
+            <a>
+              Find Distributors <Chevron />
+            </a>
+            <a>
+              Try Push in-store
+              <Chevron />
+            </a>
           </div>
         </div>
         <div className="flex flex-col w-2/5 ">
           <h3 className="font-semibold">Language and Location</h3>
-          <div className="flex gap-x-6">
-            <p>English</p>
-            <p>Mexico</p>
+          <div className="flex text-xs gap-x-0.5 ">
+            <button className="px-2 py-2 font-semibold rounded-none bg-abletonGray">
+              English
+              <img
+                className="inline-flex w-2 h-1.5 ml-12 "
+                src="images/icons/arrow.svg"
+              ></img>
+            </button>
+            <button className="flex items-center justify-between w-3/6 px-2 py-2 font-semibold rounded-none bg-abletonGray">
+              <span>United States</span>
+              <img
+                className="inline-flex w-2 h-1.5 "
+                src="images/icons/arrow.svg"
+              ></img>
+            </button>
           </div>
         </div>
       </div>

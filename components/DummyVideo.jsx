2d22ab7681a8ec2f9207f828ec93dd50fcb24f32
Cesar Camacho <icesardcm99@gmail.com>
2021-09-27 22:44:34 -0500

    Finished all arts except yellow, prevents overflowing on bigger displays

--- components/DummyVideo.jsx
+++ components/DummyVideo.jsx
@@ -2,7 +2,7 @@ export default function DummyVideo() {
   return (
     <div
       id="Dummy Video Image"
-      className="items-center w-10/12 mx-auto mt-10 lg:mt-24 lg:w-1/2 "
+      className="items-center w-10/12 mx-auto mt-10 lg:my-28 lg:w-1/2 "
     >
       <div>
         <img className="relative" src="images/video-image.png"></img>
